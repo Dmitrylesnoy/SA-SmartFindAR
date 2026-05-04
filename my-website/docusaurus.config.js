71ade6f8425df3c01f9d126a9e279a4ea1ab2a2d
Akim Lineiskiy <vasymedov@gmail.com>
2026-05-04 17:48:26 +0300

    github pages deploy config

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -4,7 +4,7 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -27,7 +27,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Akim Lineiskiy", // Usually your GitHub org/user name.
+  organizationName: "DmitryLesnoy", // Usually your GitHub org/user name.
   projectName: "SmartFindAR", // Usually your repo name.
 
   // onBrokenLinks: "throw",

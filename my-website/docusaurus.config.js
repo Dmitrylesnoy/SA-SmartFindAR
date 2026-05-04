a22d3a6d872e1155308df784077c36e8965adfbb
Akim Lineiskiy <vasymedov@gmail.com>
2026-05-04 17:28:30 +0300

    raw docs

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -15,12 +15,12 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
+  // future: {
+  //   v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  // },
 
   // Set the production url of your site here
-  url: "https://github.com/Dmitrylesnoy",
+  url: "https://DmitryLesnoy.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/SA-SmartFindAR/",
@@ -84,7 +84,6 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/facebook/docusaurus",
             label: "GitHub",
@@ -118,19 +117,6 @@ const config = {
               {
                 label: "X",
                 href: "https://x.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
